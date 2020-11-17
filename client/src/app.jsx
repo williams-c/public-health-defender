c4f20f1aa0d6aeb26d46e3050918aeb162ef2550
Chris Williams <Chris@Chriss-MacBook-Pro-2.local>
2020-11-17 14:59:38 -0700

    adds login form with password validation

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,12 +1,19 @@
 // App.jsx
 import React from 'react';
 import Title from './components/Title';
+import Login from './components/Login'
 
 const App = () => {
     return (
-        <div className="app">
+        <div className="container">
+
+          <div className="app">
 
             <Title />
+
+            <Login />
+
+          </div>
 
         </div>
     );
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,44 @@
+// App.jsx
+import React, { useState } from 'react';
+
+const Login = () => {
+    const [username, updateUsername] = useState('');
+    const [password, updatePassword] = useState('');
+
+    const validatePassword = (password) => {
+      const reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+      const test = reg.test(password);
+      return test;
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+
+      if (!validatePassword(password)) {
+        alert('Please enter a valid password')
+        return
+      }
+
+      // handle form submit, api request
+      updateUsername('')
+      updatePassword('')
+    }
+
+    return (
+      <div>
+
+        <div className='login-box'>
+
+          <form className='login-form'>
+            <input onChange={(e) => {updateUsername(e.target.value)}} className='login-form-element' type="text" placeholder="Username" value={username}></input><br/>
+            <input onChange={(e) => {updatePassword(e.target.value)}} className='login-form-element' type="password" placeholder="Password" value={password}></input><br/>
+            <input onClick={(e) => {handleSubmit(e)}} className='login-form-element' type="submit"></input>
+          </form>
+
+        </div>
+
+      </div>
+    );
+}
+
+export default Login;
