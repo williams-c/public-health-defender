81d0ed0cbf97c26cceb7e2b4bacff58eb35767f5
Chris Williams <Chris@Chriss-MacBook-Pro-2.local>
2020-11-17 14:11:10 -0700

    initial project setup, react/webpack/babel/express

---
+++ client/src/app.jsx
@@ -0,0 +1,15 @@
+// App.jsx
+import React from 'react';
+import Title from './components/Title';
+
+const App = () => {
+    return (
+        <div className="app">
+
+            <Title />
+
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/components/Title.jsx
@@ -0,0 +1,22 @@
+// App.jsx
+import React from 'react';
+
+const Title = () => {
+    return (
+      <div>
+
+        <div className='title-bar'>
+
+            <h1 className='title-text'>
+              Public Health Defender
+            </h1>
+
+            <img className="title-img" src="https://thumbs.dreamstime.com/z/sword-shield-hand-drawn-vector-illustration-simple-shadow-backdrop-behind-all-objects-separate-63669116.jpg"></img>
+
+        </div>
+
+      </div>
+    );
+}
+
+export default Title;
