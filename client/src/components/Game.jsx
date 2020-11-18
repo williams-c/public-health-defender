8c7bd244de93d03d6177098c1f950d0a5c8975e1
Chris Williams <Chris@Chriss-MacBook-Pro-2.local>
2020-11-18 16:54:41 -0700

    adds turn button and handling, adds game state

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -5,25 +5,40 @@ const sampleCities = [
   {
     name: 'Denver',
     population: 3000000,
-    infected: 0,
+    infected: Math.floor(Math.random() * 1000),
     spread: 5,
     income: 1000,
   },
   {
     name: 'Seattle',
     population: 4000000,
-    infected: 0,
+    infected: Math.floor(Math.random() * 1000),
     spread: 7,
     income: 1500,
   },
 ]
 
 const Game = () => {
+  const [gameState, updateGameState] = useState('playing');
   const [cities, updateCities] = useState(sampleCities);
+
+  const handleTurn = () => {
+    let newCities = cities;
+    newCities.forEach((city) => {
+      const increase = Math.ceil((city.spread / 100) * city.infected)
+      city.infected += increase
+      if (city.infected >= city.population) {
+        updateGameState('defeat')
+      }
+    })
+    updateCities([...newCities])
+  }
 
   return (
     <div>
+      {gameState === 'defeat' ? <div> Game Over ! </div> : ''}
       <CityMenu cities={cities}/>
+      <button onClick={handleTurn} className="turn-btn">End Turn</button>
     </div>
   )
 }
